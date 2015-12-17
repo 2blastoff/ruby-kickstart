c2a4d82681c241f4c604a7271f45e2b779213017
blastoff <blastoff2@hotmail.com>
2015-12-17 16:27:58 +0000

    updated learning7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -3,6 +3,14 @@
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
-
 def pirates_say_arrrrrrrrr(string)
+   newy =""
+   
+   var = string.split(//)
+  
+    array = var
+    array.each_with_index {|val, index| newy << array.at(index + 1).to_s if val.match(/["r","R"]/) }
+   return newy
+
 end
+
